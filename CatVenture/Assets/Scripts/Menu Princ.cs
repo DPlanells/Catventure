4f804b5b81c89f31b30f135fd59e86fea958297d
AngelOscuro20 <125664399+AngelOscuro20@users.noreply.github.com>
2024-11-22 20:08:15 +0100

    Animaciones menu principal

--- CatVenture/Assets/Scripts/Menu Princ.cs
+++ CatVenture/Assets/Scripts/Menu Princ.cs
@@ -5,11 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrinc : MonoBehaviour
 {
-    // Start is called before the first frame update
-   public void NuevaPartida()
+
+    public Animator MCamera;
+
+    private void Start()
+    {
+        MCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+    }
+// Start is called before the first frame update
+    public void NuevaPartida()
 
     {
     SceneManager.LoadScene("MainScene");
+    }
+    public void CargarPartida()
+    {
+        Debug.Log(MCamera.gameObject);
+        MCamera.Play("Continuar");
+    }
+    public void Creditos()
+    {
+        Debug.Log(MCamera.gameObject);
+        MCamera.Play("Creditos"); 
     }
 
 }
