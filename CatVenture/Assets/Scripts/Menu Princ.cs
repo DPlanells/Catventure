859ed7ff1c0a1e77f0771c6293dfe10fb4ad34d3
AngelOscuro20 <125664399+AngelOscuro20@users.noreply.github.com>
2024-12-03 19:18:35 +0100

    Creditos

--- CatVenture/Assets/Scripts/Menu Princ.cs
+++ CatVenture/Assets/Scripts/Menu Princ.cs
@@ -7,10 +7,21 @@ public class MenuPrinc : MonoBehaviour
 {
 
     public Animator MCamera;
+    public Animator MUIChild1;
+    public Animator MUIChild2;
+    public Animator MUIChild3;
+    public Canvas MUI;
+    public Canvas CredUI;
+    public Animator CredAnim;
 
     private void Start()
     {
         MCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+        MUI = GameObject.FindGameObjectWithTag("MainMenu").GetComponent<Canvas>();
+        CredUI = GameObject.FindGameObjectWithTag("Creditos").GetComponent<Canvas>();
+        MCamera.SetBool("Creditos", false);
+        CredUI.enabled = false;
+
     }
 // Start is called before the first frame update
     public void NuevaPartida()
@@ -26,7 +37,24 @@ public class MenuPrinc : MonoBehaviour
     public void Creditos()
     {
         Debug.Log(MCamera.gameObject);
-        MCamera.Play("Creditos"); 
+        
+        MUI.enabled = false;
+        MCamera.SetBool("Creditos", true);
+        CredUI.enabled = true;
+        CredAnim.SetTrigger("creditos");
+
+
+    }
+    public void VolverPrincipal()
+    {
+        MCamera.SetBool("Creditos", false);
+        CredAnim.SetTrigger("creditosvolver");
+        CredUI.enabled = false;
+        Debug.Log(MUIChild1);
+        MUIChild1.SetTrigger("trns");
+        MUIChild2.SetTrigger("trns");
+        MUIChild3.SetTrigger("trns");
+        MUI.enabled = true;
     }
 
 }
