957c23c9f6b0fc731af72dc76e3248045266580e
DPlanells <davidplanells2@gmail.com>
2024-12-19 20:07:23 +0100

    Enemigo Planta Venenosa
    Se ha implementado un nuevo prefab, el de una planta venenosa, con su script pertinente.
    
    La planta no se mueve, ni es sólida, pero si el jugador la pisa, pierde una vida

---
+++ CatVenture/Assets/Scripts/PlantaMalaScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantaMalaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.instance.dañarJugador(1);
+            Debug.Log("Se ha dañado al jugador por planta");
+        }
+    }
+}
