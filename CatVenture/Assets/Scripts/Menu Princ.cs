10bcb2c89e9f161b2a10bc045f6a1fdc6843b912
AngelOscuro20 <125664399+AngelOscuro20@users.noreply.github.com>
2025-01-21 00:02:06 +0100

    Guardados UI 1

--- CatVenture/Assets/Scripts/Menu Princ.cs
+++ CatVenture/Assets/Scripts/Menu Princ.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuPrinc : MonoBehaviour
 {
@@ -18,14 +19,30 @@ public class MenuPrinc : MonoBehaviour
     public Animator CredAnim;
     public Animator BotonVolver;
     public Button IniciarBoton;
+    public Button botonslot1;
+    public Button botonslot2;
+    public Button botonslot3;
     public Button VolverBoton;
     public Animator Partida1;
     public Animator Partida2;
     public Animator Partida3;
     int tiempo;
     private int Slot = 1;
-
-
+    private int livesslot1;
+    private int livesslot2;
+    private int livesslot3;
+    private int coinsslot1;
+    private int coinsslot2;
+    private int coinsslot3;
+    private SaveData info1;
+    private SaveData info2;
+    private SaveData info3;
+    public TMP_Text UIVidasText1;
+    public TMP_Text UIVidasText2;
+    public TMP_Text UIVidasText3;
+    public TMP_Text UICoinsText1;
+    public TMP_Text UICoinsText2;
+    public TMP_Text UICoinsText3;
 
     private void Start()
     {
@@ -55,8 +72,8 @@ public class MenuPrinc : MonoBehaviour
 
     public void Update() {
         bool MUIBool = MUI.enabled;
-        if (Input.GetKeyDown(KeyCode.Return) && MUIBool) { 
-            IniciarBoton.onClick.Invoke(); 
+        if (Input.GetKeyDown(KeyCode.Return) && MUIBool) {
+            IniciarBoton.onClick.Invoke();
         }
         bool CredBool = CredUI.enabled;
         if (Input.GetKeyDown(KeyCode.Escape) && CredBool)
@@ -67,13 +84,13 @@ public class MenuPrinc : MonoBehaviour
 
     public void CoroutineTimer(int tiempo)
     {
-        
+
         StartCoroutine(timer(tiempo));
     }
 
     private IEnumerator timer(int tiempo)
     {
-        yield return new WaitForSeconds(tiempo/2);
+        yield return new WaitForSeconds(tiempo / 2);
         MUI.enabled = false;
         yield return new WaitForSeconds(tiempo);
         SeleccionarSlot(Slot);
@@ -86,11 +103,27 @@ public class MenuPrinc : MonoBehaviour
         MCamera.SetTrigger("IniciarPartida");
         tiempo = 3;
         CoroutineTimer(tiempo);
-        
-        
+
+
     }
     public void CargarPartida()
     {
+        SaveManager saveManager = GameObject.FindObjectOfType<SaveManager>();
+        info1 = getInfoSlot(1);
+        info2 = getInfoSlot(2);
+        info3 = getInfoSlot(3);
+        if (saveManager.SaveSlotExists(1)) { 
+            UIVidasText1.text = info1.lives.ToString();
+            UICoinsText1.text = info1.coins.ToString();
+        } else { botonslot1.interactable = false; }
+        if (saveManager.SaveSlotExists(2)) { 
+            UIVidasText2.text = info2.lives.ToString();
+            UICoinsText2.text = info2.coins.ToString();
+        } else { botonslot2.interactable = false; }
+        if (saveManager.SaveSlotExists(3)) { 
+            UIVidasText3.text = info3.lives.ToString();
+            UICoinsText3.text = info3.coins.ToString();
+        } else { botonslot3.interactable = false; }
         MCamera.SetBool("Cargar", true);
         CargarUI.enabled = true;
         CredUI.enabled = false;
@@ -100,6 +133,8 @@ public class MenuPrinc : MonoBehaviour
         Partida2.SetTrigger("trns");
         Partida3.SetTrigger("trns");
     }
+    
+
     public void Creditos()
     {
         Debug.Log(MCamera.gameObject);
@@ -137,7 +172,7 @@ public class MenuPrinc : MonoBehaviour
         Application.Quit();
     }
 
-    private SaveData getInfoSlot(int slot)
+    public SaveData getInfoSlot(int slot)
     {
         SaveManager saveManager = GameObject.FindObjectOfType<SaveManager>();
         if (saveManager == null)
@@ -147,6 +182,7 @@ public class MenuPrinc : MonoBehaviour
         }
         else
         {
+            Debug.Log("cargado");
             SaveData info = saveManager.LoadGame(slot);
             return info;
         }
